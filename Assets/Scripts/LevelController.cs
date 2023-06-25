7777cb2d090699111d56a4d70856226bf1ceb1ba
Paulo Daniel <pdaniel@gmail.com>
2023-06-25 11:47:06 -0400

    level design

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] gameObjects;
     private Transform playerTransform;
-    [SerializeField] private float roadSize;
-    [SerializeField] private float limit;
+    [SerializeField] private float limit = 2086f;
 
     private void Start()
     {
         playerTransform = GameObject.Find("Player").GetComponent<Transform>();
-        gameObjects = GameObject.FindGameObjectsWithTag("Road");
-        GetRoadSize();
-        SetLimitToRestartPostion();
-        
     }
 
     private void Update()
@@ -29,19 +23,6 @@ public class LevelManager : MonoBehaviour
         {
             playerTransform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, 0);
         }
-    }
-
-    private void GetRoadSize()
-    {
-        foreach (var go in gameObjects)
-        {
-            roadSize += go.GetComponent<BoxCollider>().bounds.size.z;
-        }
-    }
-
-    private void SetLimitToRestartPostion()
-    {
-        limit = roadSize * 0.9f;
     }
 
 }

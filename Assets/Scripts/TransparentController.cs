e5e3d1eb9275d5fd8adfbd5e6a556c5ad8dc0ced
Paulo Daniel <pdaniel@gmail.com>
2023-07-07 14:19:30 -0400

    Transparency test

--- Assets/Scripts/TransparentController.cs
+++ Assets/Scripts/TransparentController.cs
@@ -7,8 +7,10 @@ public class TransparentController : MonoBehaviour
 
     private CharacterController characterController;
     [SerializeField] private float raycastDistance = 3f;
+    [SerializeField] private float alpha = 0.5f;
 
     private RaycastHit hitInfo;
+    private Vector3 origin;
 
     void Start()
     {
@@ -17,16 +19,39 @@ public class TransparentController : MonoBehaviour
 
     void Update()
     {
-        
+        origin = new Vector3(characterController.transform.position.x, 1.5f, characterController.transform.position.z);
+        CheckObstacle();
     }
 
     private void CheckObstacle()
     {
-        Ray ray = new Ray(characterController.transform.position, Vector3.forward);
+        Ray ray = new Ray(origin, Vector3.forward);
 
         if (Physics.Raycast(ray, out hitInfo, raycastDistance))
         {
+            Debug.Log("Hit: " + hitInfo.transform.name);
 
+            if (hitInfo.transform.CompareTag("Obstacle"))
+            {
+                Debug.Log("Hit: " + hitInfo.transform.name);
+                SetTransparency();
+            }
         }
+    }
+
+    private void SetTransparency()
+    {
+        Renderer renderer = hitInfo.transform.GetComponent<Renderer>();
+        renderer.material.SetFloat(Shader.PropertyToID("_Alpha"), alpha);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 sphereDirection = Vector3.forward;
+
+        // Draw the sphere cast shape
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawRay(origin, sphereDirection * raycastDistance);
+
     }
 }
